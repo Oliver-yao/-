2f7db17af48f0ca32c469b9f4d905dc50b413ac0
Oliver-yao <58976031+Oliver-yao@users.noreply.github.com>
2020-01-13 16:47:15 +0800

    案例

---
+++ 14.py
@@ -0,0 +1,53 @@
+import threading
+import time
+
+lock_1 = threading.Lock()
+lock_2 = threading.Lock()
+
+def Func_1():
+    print("func_1 starting...")
+    lock_1.acquire()
+    print("func_1 申请了 lock_1")
+    time.sleep(2)
+    print("func_1 等待 lock_2")
+    lock_2.acquire()
+    print("func_1 申请了 lock_2")
+
+    lock_2.release()
+    print("func_1 释放了 lock_2")
+
+    lock_1.release()
+    print("func_1 释放了 lock_1")
+
+    print("func_1 Done...")
+
+
+def Func_2():
+    print("func_2 starting...")
+    lock_2.acquire()
+    print("func_2 申请了 lock_2")
+    time.sleep(4)
+    print("func_2 等待 lock_2")
+    lock_1.acquire()
+    print("func_2 申请了 lock_1")
+
+    lock_1.release()
+    print("func_2 释放了 lock_1")
+
+    lock_2.release()
+    print("func_2 释放了 lock_2")
+
+    print("func_2 Done...")
+
+if __name__ == '__main__':
+    print("主程序启动。。。")
+    t1 = threading.Thread(target=Func_1, args=())
+    t2 = threading.Thread(target=Func_2, args=())
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print("主程序结束")
